4c5a5d27d79dffa7d0e0017ce1ec8381ff011114
youcancallmejay <148828634+youcancallmejay@users.noreply.github.com>
2023-11-19 22:08:18 -0600

    Initial commit.
    Will add the rest of the consonants in coming commits. Working on "play audio" feature when user guesses wrong to learn what letter they chose.

---
+++ script.js
@@ -0,0 +1,108 @@
+//click button to start game
+//show three cards with thai letters
+//play sound clip of random thai letter
+//choose letter based on sound
+//if correct, green
+//if wrong, red
+//reset cards
+//play now sound clip of random thai letter
+
+let audio = new Audio();
+
+let cards = [];
+
+function startGame() {
+  pickThree();
+  cardReset();
+  printCards();
+  quizCard = randomSelection(cards);
+  playSound(quizCard);
+  userGuess();
+}
+
+function cardReset() {
+  const cards = document.querySelectorAll(".the-card");
+  cards.forEach((card) => {
+    card.classList.remove("flipped");
+    card.style.backgroundColor = "white";
+  });
+}
+
+function userGuess() {
+  const cards = document.querySelectorAll(".the-card");
+
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      let cardText = card.querySelector(".the-front p").innerText.trim();
+      if (cardText == quizCard) {
+        card.style.backgroundColor = "green";
+        playSound(quizCard);
+        setTimeout(() => {
+          startGame();
+        }, 4000);
+      } else {
+        card.classList.toggle("flipped");
+        card.style.backgroundColor = "red";
+      }
+    });
+  });
+}
+
+function printCards() {
+  let card1 = cards[0];
+  let card2 = cards[1];
+  let card3 = cards[2];
+  const firstCard = document.querySelector("#first-card .the-front p");
+  firstCard.innerHTML = card1;
+  const secondCard = document.querySelector("#second-card .the-front p");
+  secondCard.innerHTML = card2;
+  const thirdCard = document.querySelector("#third-card .the-front p");
+  thirdCard.innerHTML = card3;
+}
+
+function randomSelection(cards) {
+  index = Math.floor(Math.random() * cards.length);
+  letter = cards[index];
+  return letter;
+}
+
+function pickThree() {
+  let card = "";
+  for (i = 0; i < 3; i++) {
+    do {
+      card = pickRandom();
+    } while (cards.includes(card));
+
+    cards[i] = card;
+  }
+}
+
+function playSound(quizCard) {
+  soundSource = audioSamples[quizCard];
+  audio.src = soundSource;
+  audio.play();
+}
+
+const audioSamples = {
+  กไก่: "audio/กไก่.mp3",
+  ฅคน: "audio/ฅคน.mp3",
+  ฉฉิ่ง: "audio/ฉฉิ่ง.mp3",
+  ญหญิง: "audio/ญหญิง.mp3",
+  ฑมณโฑ: "audio/ฑมณโฑ.mp3",
+  หหีบ: "audio/หหีบ.mp3",
+  วแหวน: "audio/วแหวน.mp3",
+  มม้า: "audio/มม้า.mp3",
+  ฝฝา: "audio/ฝฝา.mp3",
+  นหนู: "audio/นหนู.mp3",
+  ตเต่า: "audio/ตเต่า.mp3",
+};
+
+function pickRandom() {
+  const keys = Object.keys(audioSamples);
+  index = Math.floor(Math.random() * keys.length);
+  return keys[index];
+}
+
+function playAudio() {
+  playSound(quizCard);
+}
